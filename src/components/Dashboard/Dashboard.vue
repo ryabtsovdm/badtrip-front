2adf33ab38f2fdf94a6a350db406c081ce52bda7
mishantrop <quasi-art@ya.ru>
2018-10-28 07:58:08 +0300

    Pravki

--- src/components/Dashboard/Dashboard.vue
+++ src/components/Dashboard/Dashboard.vue
@@ -1,8 +1,59 @@
-
 <script>
+import axios from "axios";
+
+const host = "http://cv15621.tmweb.ru";
+
 export default {
   name: "dashboard",
-  components: {}
+  computed: {
+    user() {
+      return this.$store.getters.getUser;
+    }
+  },
+  data: () => ({
+    tab: "profile",
+    trips: [],
+    props: []
+  }),
+  methods: {
+    setTrips: function(trips) {
+      this.trips = trips;
+    },
+    setProps: function(props) {
+      this.props = props;
+    },
+    setTab: function(tab) {
+      this.tab = tab;
+    },
+    createNew: function() {
+      if (this.tab === "trips") {
+        this.$router.push("/new/trip");
+      } else {
+        this.$router.push("/new/prop");
+      }
+    }
+  },
+  created() {
+    const Authorization = localStorage.getItem("token");
+
+    axios({
+      url: `${host}/api/trips/my`,
+      headers: {
+        "content-type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${Authorization}`
+      }
+    }).then(({ data }) => this.setTrips(data.trips));
+
+    axios({
+      url: `${host}/api/projects/my`,
+      headers: {
+        "content-type": "application/json",
+        Accept: "application/json",
+        Authorization: `Bearer ${Authorization}`
+      }
+    }).then(({ data }) => this.setProps(data.projects));
+  }
 };
 </script>
 
@@ -13,9 +64,28 @@ export default {
     </div>
     <div class="main">
       <div class="menu">
-        <div class="option">Профиль</div>
-        <div class="option">Мои предложения</div>
-        <div class="option">Мои путешествия</div>
+        <div v-bind:class="{ active: tab === 'profile', option: true }" v-on:click="setTab('profile')">Профиль</div>
+        <div v-bind:class="{ active: tab === 'props', option: true }" v-on:click="setTab('props')">Мои предложения</div>
+        <div v-bind:class="{ active: tab === 'trips', option: true }" v-on:click="setTab('trips')">Мои путешествия</div>
+      </div>
+      <div class="content">
+        <div class="profile" v-if="tab === 'profile'">
+          <div class="param"><label class="label">E-mail: </label><input class="inputField" type="email" name="email" v-model="user.email" disabled /></div>
+          <div class="param"><label class="label">Пароль: </label><input class="inputField" type="password" name="password" disabled /></div>
+        </div>
+        <div class="list" v-if="tab === 'props'">
+          <template v-for="(prop) in props">
+            <div :key="prop.id" class="prop">{{ prop.text }}</div>
+          </template>
+        </div>
+        <div class="list" v-if="tab === 'trips'">
+          <template v-for="(trip) in trips">
+            <div :key="trip.id" class="trip">{{ trip.text }}</div>
+          </template>
+        </div>
+        <div class="createButton" v-if="tab !== 'profile'" v-on:click="createNew()">
+          Создать
+        </div>
       </div>
     </div>
   </div>
@@ -50,9 +120,43 @@ export default {
 .option {
   border-bottom: 1px solid #888;
   padding: 0.3rem 0.5rem;
+  opacity: 0.5;
+}
+
+.active {
+  opacity: 1;
+  box-shadow: #888;
 }
 
 .option:last-child {
   border: none;
+}
+
+.param {
+  width: 400px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0.3rem 0;
+}
+
+.inputField {
+  color: #000;
+  width: 50%;
+  border: 1px solid #ddd;
+  padding: 0.5rem;
+  border-radius: 0.3rem;
+}
+
+.label {
+  width: 50%;
+}
+
+.createButton {
+  background-color: ghostwhite;
+  padding: 0.8rem;
+  color: lightcoral;
+  font-size: 1.5rem;
+  font-weight: 700;
 }
 </style>
